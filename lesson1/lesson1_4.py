14189f1d0ab80e25ac48d07ed337735723115968
inoue-comp <s104242@hyogo-c.ed.jp>
2025-06-20 11:49:55 +0900

    Add files via upload
    レッスン1

---
+++ lesson1/lesson1_4.py
@@ -0,0 +1,20 @@
+##第一回　3
+##"#"から始まる文章は読み取られません。コメントなどに利用できます。
+
+##print()関数の使用
+##3 変数を使う
+
+name ="たろう"
+num1 = 2
+num2 = 3
+print("じろう")
+print(name)
+print("name")
+print(num1)
+print(num2)
+print(num1+num2)
+print("こんにちは、"+name+"さん!")
+print(num1*num2)
+
+
+##Q.関数の使い方に違いはありますか?
